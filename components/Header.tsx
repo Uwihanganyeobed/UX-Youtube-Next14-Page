1a3960acfe746d18ce64a5efe162bc5b04fa4b52
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-08-15 10:18:48 +0200

    finalising

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,9 @@ const Header: React.FC = () => {
         <Link href='/'>
         <FaBars className="h-7 mx-4 text-gray-700" />
         </Link>
+        <Link href='/'>
         <img src="https://i.imgur.com/EIrHodL.jpg" alt="YouTube Logo" className="h-14" />
+        </Link>
       </div>
       <div className="flex items-center flex-grow max-w-lg mx-6">
         <input 
@@ -26,15 +28,15 @@ const Header: React.FC = () => {
       </div>
       <div className="flex items-center space-x-6 pr-6">
         <div className="relative group">
-          <FaVideo className="h-7 text-gray-700" />
+          <FaVideo className="h-7 text-gray-700 cursor-pointer" />
           <span className="absolute left-1/2 transform -translate-x-1/2 mt-2 opacity-0 group-hover:opacity-100 text-xs bg-gray-700 text-white p-1 rounded">Create</span>
         </div>
-        <FaTh className="h-7 text-gray-700" />
+        <FaTh className="h-7 text-gray-700 cursor-pointer" />
         <div className="relative">
-          <FaBell className="h-7 text-gray-700" />
+          <FaBell className="h-7 text-gray-700 cursor-pointer" />
           <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1">3</span>
         </div>
-        <img src="https://i.imgur.com/8h8FIqj.jpg" alt="User Profile" className="h-8 rounded-xl " />
+        <img src="https://i.imgur.com/8h8FIqj.jpg" alt="User Profile" className="h-8 rounded-xl cursor-pointer" />
       </div>
     </header>
   );
